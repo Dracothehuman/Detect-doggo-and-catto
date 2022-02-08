e2eff788363a850dcd49fdaf0c5c15d58a8840fc
Dracothehuman <80511020+Dracothehuman@users.noreply.github.com>
2022-02-08 18:18:28 +1100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,51 @@
+img="";
+status="";
+object = [];
+
+function preload(){
+    img = loadImage("dog_cat.jpg");
+}
+
+function setup(){ 
+    canvas = createCanvas(780, 540);
+    canvas.center()
+    objectDetector=ml5.objectDetector('cocossd', modelLoaded);
+    document.getElementById("status").innerHTML ="Status:Detecting Image";
+}
+
+function draw(){
+
+    image(img , 0 , 0, 780, 540);
+    if(status != ""){
+        
+        for(i=0; i < object.length; i++){
+            document.getElementById("status").innerHTML = "Status = Objects Detected.";
+
+            fill("#FF0000");
+            percent = floor(object[i].confidence * 100);
+            text(object[i].label + "" + percent + "%" ,object[i].x, object[i].y);
+            noFill();
+            stroke("#FF0000");
+            rect(object[i].x, object[i].y, object[i].width, object[i].height);
+        }
+
+    }
+}
+
+function modelLoaded(){
+    console.log("Modal has loaded");
+    status= true;
+    objectDetector.detect(img, gotResult);
+}
+function gotResult(error, results){
+    if(error){
+        console.log(error);
+    }
+    console.log(results);
+    object = results;
+    
+    
+    }
+    
+
+
